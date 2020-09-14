d2eb48f2d6700caa20613e125b2eae46c1b3af3d
imjchiang <jchiang296@gmail.com>
2020-09-14 11:58:02 -0700

    [03-bank.py] challenge complete

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,36 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+balance = input("Your balance is\n")
+balance = int(balance)
+
+def complete():
+    finish = input("Are you done? (Y/N)\n")
+
+    if (finish == "Y"):
+        print("Have a nice day!")
+    elif (finish == "N"):
+        do_something(balance)
+    else:
+        print("INVALID INPUT")
+        complete()
+
+def do_something(balance):
+    options = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+
+    if (options == "deposit"):
+        deposit = input("How much would you like to deposit?\n")
+        deposit = int(deposit)
+        balance += deposit
+    elif (options == "withdraw"):
+        withdraw = input("How much would you like to withdraw?\n")
+        withdraw = int(withdraw)
+        balance -= withdraw
+    elif (options == "check_balance"):
+        print("Your balance is {}".format(balance))
+    else:
+        print("INVALID INPUT")
+
+    complete()
+    
+
+do_something(balance)
